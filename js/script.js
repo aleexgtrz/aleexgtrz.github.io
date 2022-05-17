57e0783e20cf136d56327390a45e80995d913500
aleexgtrz <105662365+aleexgtrz@users.noreply.github.com>
2022-05-17 20:19:43 +0200

    Major changes in js

--- js/script.js
+++ js/script.js
@@ -19,32 +19,39 @@ const projects = [
 		url:'./images/memory.jpg',
 		category: 'Javascript',
         git: 'https://github.com/aleexgtrz/pokemon_memory_game',
+		live: 'https://magenta-sorbet-ef9877.netlify.app/',
         name: 'Pokémon Memory Game'
 	},
-    {
-		url: './images/puzzle.png',
+	{
+		url:'./images/puzzle.png',
 		category: 'Javascript',
         git: 'https://github.com/aleexgtrz/pokemon_memory_game',
+		live: 'https://magenta-sorbet-ef9877.netlify.app/',
         name: 'Puzzle Maker'
 	},
-    {
-		url: './images/game_of_life.jpg',
-		category: 'Java',
-        git: 'https://github.com/aleexgtrz/pokemon_memory_game',
-        name: 'Game of life'
-	},
-    {
-		url: './images/calculator.png',
+	{
+		url:'./images/calculator.png',
 		category: 'Javascript',
         git: 'https://github.com/aleexgtrz/pokemon_memory_game',
+		live: 'https://magenta-sorbet-ef9877.netlify.app/',
         name: 'Calculator'
-	},		
+	},
+	
 ];
 
 const renderProjects = category => {
 	projects.forEach(project => {
 		if (project.category === category) {
-            $(".row").append(`<div class='col-sm' onclick = 'window.open("${project.git}")' style = "background-image: url(${project.url});">${project.name}</div>`)
+            $(".row").append(`
+					<div class='col-sm' style = "background-image: url(${project.url});">
+						${project.name} <br>
+
+						<div style = "position: absolute; bottom: 20px;"> 
+							<button style = "border-radius: 10px" onclick = 'window.open("${project.live}")'>Live</button>
+							<button style = "border-radius: 10px" onclick = 'window.open("${project.git}")'>Git</button>
+						</div>
+					</div>
+			`)
 		}
 	});
 };
